9377a0a67a21322ab8c16f238a3f3a1da31bd803
Chris Becker <Cbeck527@gmail.com>
2013-07-02 18:54:43 -0400

    initial color detection based on input being 3 or 6 characters

--- whatsthiscolor.py
+++ whatsthiscolor.py
@@ -3,13 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def isHex(inputValue=""):
+    if len(inputValue) == 3 or len(inputValue) == 6:
+        return True
+    else:
+        return False
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 @app.route('/<inputColor>')
 def returnColor(inputColor):
-    return 'The color was #%s' % inputColor
+    if isHex(inputColor):
+        return 'The color was #%s' % inputColor
+    else:
+        return 'Not a valid hex color'
 
 if __name__ == '__main__':
     app.run(debug=True)
