fc4eae8a24704080d436f0f8a678b8ff159276f9
Chris Becker <Cbeck527@gmail.com>
2013-07-02 22:34:32 -0400

    color page and favicon fun

--- whatsthiscolor.py
+++ whatsthiscolor.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, send_file, request
+from PIL import Image
+import StringIO
 
 app = Flask(__name__)
 
@@ -19,15 +21,27 @@ def isHex(inputValue=""):
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/<inputColor>')
 def returnColor(inputColor):
     color = isHex(inputColor)
     if color:
-        return '<style>body{background-color:#%s;}</style>The color is #%s' % (color, color)
+        return render_template('color.html', color=color)
     else:
         return 'Not a valid hex color'
+
+@app.route('/favicon.ico')
+def returnFavicon():
+    color = request.args.get('color')
+    r = int(color[:2], 16)
+    g = int(color[2:4], 16)
+    b = int(color[4:6], 16)
+    img = Image.new('RGB', (16, 16), (r, g, b))
+    img_io = StringIO.StringIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(debug=True)
